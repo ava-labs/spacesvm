008c0118407ac44c550fd7ead3b950d01412deb4
Patrick O'Grady <prohb125@gmail.com>
2021-12-07 23:00:16 -0800

    update claim_tx

--- chain/claim_tx.go
+++ chain/claim_tx.go
@@ -21,22 +21,15 @@ func (c *ClaimTx) Execute(db database.Database, blockTime int64) error {
 	if len(c.Prefix) == crypto.PublicKeySize && !bytes.Equal(c.Sender[:], c.Prefix) {
 		return ErrPublicKeyMismatch
 	}
-	i, has, err := GetPrefixInfo(db, c.Prefix)
+	previousInfo, has, err := GetPrefixInfo(db, c.Prefix)
 	if err != nil {
 		return err
 	}
-	if !has {
-		return c.accept(db, blockTime)
-	}
-	if i.Expiry >= blockTime {
+	if has && previousInfo.Expiry >= blockTime {
 		return ErrPrefixNotExpired
 	}
-	return c.accept(db, blockTime)
-}
-
-func (c *ClaimTx) accept(db database.Database, blockTime int64) error {
-	i := &PrefixInfo{Owner: c.Sender, LastUpdated: blockTime, Expiry: blockTime + expiryTime, Keys: 1}
-	if err := PutPrefixInfo(db, c.Prefix, i); err != nil {
+	newInfo := &PrefixInfo{Owner: c.Sender, LastUpdated: blockTime, Expiry: blockTime + expiryTime, Keys: 1}
+	if err := PutPrefixInfo(db, c.Prefix, newInfo); err != nil {
 		return err
 	}
 	// Remove anything that is stored in value prefix
--- chain/storage.go
+++ chain/storage.go
@@ -3,6 +3,7 @@ package chain
 import (
 	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
 // 0x0/ (singleton prefix info)
@@ -27,21 +28,41 @@ var (
 )
 
 func PrefixInfoKey(prefix []byte) []byte {
-	return append([]byte{infoPrefix, PrefixDelimiter}, prefix...)
+	b := make([]byte, len(prefix)+2)
+	packer := wrappers.Packer{Bytes: b}
+	packer.PackByte(infoPrefix)
+	packer.PackByte(PrefixDelimiter)
+	packer.PackBytes(prefix)
+	return b
 }
 
 func PrefixValueKey(prefix []byte, key []byte) []byte {
-	b := append([]byte{keyPrefix, PrefixDelimiter}, prefix...)
-	b = append(b, PrefixDelimiter)
-	return append(b, key...)
+	b := make([]byte, len(prefix)+len(key)+3)
+	packer := wrappers.Packer{Bytes: b}
+	packer.PackByte(keyPrefix)
+	packer.PackByte(PrefixDelimiter)
+	packer.PackBytes(prefix)
+	packer.PackByte(PrefixDelimiter)
+	packer.PackBytes(key)
+	return b
 }
 
 func PrefixTxKey(txID ids.ID) []byte {
-	return append([]byte{txPrefix, PrefixDelimiter}, txID[:]...)
+	b := make([]byte, len(txID)+2)
+	packer := wrappers.Packer{Bytes: b}
+	packer.PackByte(txPrefix)
+	packer.PackByte(PrefixDelimiter)
+	packer.PackBytes(txID[:])
+	return b
 }
 
 func PrefixBlockKey(blockID ids.ID) []byte {
-	return append([]byte{blockPrefix, PrefixDelimiter}, blockID[:]...)
+	b := make([]byte, len(blockID)+2)
+	packer := wrappers.Packer{Bytes: b}
+	packer.PackByte(blockPrefix)
+	packer.PackByte(PrefixDelimiter)
+	packer.PackBytes(blockID[:])
+	return b
 }
 
 func GetPrefixInfo(db database.Database, prefix []byte) (*PrefixInfo, bool, error) {
