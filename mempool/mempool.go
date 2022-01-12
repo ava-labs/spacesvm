e13829d4996020aad41ea57c04a86d8b1101f0ae
StephenButtolph <stephen@avalabs.org>
2022-01-12 15:45:39 -0500

    split out tx heap from mempool

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -5,7 +5,6 @@ package mempool
 
 import (
 	"container/heap"
-	"fmt"
 	"sync"
 
 	"github.com/ava-labs/avalanchego/ids"
@@ -14,76 +13,6 @@ import (
 )
 
 var _ chain.Mempool = &Mempool{}
-
-// txEntry is used to track the work transactions pay to be included in
-// the mempool.
-type txEntry struct {
-	id         ids.ID
-	tx         *chain.Transaction
-	difficulty uint64
-	index      int
-}
-
-// internalTxHeap is used to track pending transactions by [difficulty]
-type internalTxHeap struct {
-	isMinHeap bool
-	items     []*txEntry
-	lookup    map[ids.ID]*txEntry
-}
-
-func newInternalTxHeap(items int, isMinHeap bool) *internalTxHeap {
-	return &internalTxHeap{
-		isMinHeap: isMinHeap,
-		items:     make([]*txEntry, 0, items),
-		lookup:    map[ids.ID]*txEntry{},
-	}
-}
-
-func (th internalTxHeap) Len() int { return len(th.items) }
-
-func (th internalTxHeap) Less(i, j int) bool {
-	if th.isMinHeap {
-		return th.items[i].difficulty < th.items[j].difficulty
-	}
-	return th.items[i].difficulty > th.items[j].difficulty
-}
-
-func (th internalTxHeap) Swap(i, j int) {
-	th.items[i], th.items[j] = th.items[j], th.items[i]
-	th.items[i].index = i
-	th.items[j].index = j
-}
-
-func (th *internalTxHeap) Push(x interface{}) {
-	entry, ok := x.(*txEntry)
-	if !ok {
-		panic(fmt.Errorf("unexpected %T, expected *txEntry", x))
-	}
-	if th.Has(entry.id) {
-		return
-	}
-	th.items = append(th.items, entry)
-	th.lookup[entry.id] = entry
-}
-
-func (th *internalTxHeap) Pop() interface{} {
-	n := len(th.items)
-	item := th.items[n-1]
-	th.items[n-1] = nil // avoid memory leak
-	th.items = th.items[0 : n-1]
-	delete(th.lookup, item.id)
-	return item
-}
-
-func (th *internalTxHeap) Get(id ids.ID) (*txEntry, bool) {
-	entry, ok := th.lookup[id]
-	return entry, ok
-}
-
-func (th *internalTxHeap) Has(id ids.ID) bool {
-	_, has := th.Get(id)
-	return has
-}
 
 type Mempool struct {
 	mu      sync.RWMutex
