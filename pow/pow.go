8cb6cb855d2b0bd5f58daad41832bd1fc03e3b46
Patrick O'Grady <prohb125@gmail.com>
2022-01-12 00:33:33 -0800

    fix expected hashes

--- pow/pow.go
+++ pow/pow.go
@@ -11,26 +11,34 @@ import (
 )
 
 // Recommended reading to understand how this works: https://en.bitcoin.it/wiki/Difficulty
-var (
+const (
+	totalPrecision = 256
 	// Bitcoin uses 2^208 for the [base]
 	// Each unit of difficulty at 2^230 adds ~1ms on a new laptop
-	base          = new(big.Int).Exp(big.NewInt(2), big.NewInt(230), nil)
-	scalingFactor = new(big.Int).Mul(big.NewInt(0xFFFF), base)
+	difficultyBase = 230
+	expectedBase   = totalPrecision - difficultyBase
+)
+
+var (
+	big2           = big.NewInt(2)
+	scalingOperand = big.NewInt(0xFFFF)
+
+	diffFactor = new(big.Int).Mul(scalingOperand, new(big.Int).Exp(big2, big.NewInt(difficultyBase), nil))
+
+	expectedFactor = new(big.Int).Exp(big2, big.NewInt(expectedBase), nil)
 )
 
 func Difficulty(b []byte) uint64 {
 	h := sha3.Sum256(b)
 	v := new(big.Int).SetBytes(h[:])
-	r := new(big.Int).Div(scalingFactor, v)
+	r := new(big.Int).Div(diffFactor, v)
 	return r.Uint64()
 }
 
 // ExpectedHashes provides an estimate of the number of hashes that must be
 // computed for a given difficulty.
 func ExpectedHashes(difficulty uint64) uint64 {
-	// 256-240 = 16
-	b := new(big.Int).Exp(big.NewInt(2), big.NewInt(16), nil)
-	n := new(big.Int).Mul(new(big.Int).SetUint64(difficulty), b)
-	r := new(big.Int).Div(n, big.NewInt(0xFFFF))
+	n := new(big.Int).Mul(new(big.Int).SetUint64(difficulty), expectedFactor)
+	r := new(big.Int).Div(n, scalingOperand)
 	return r.Uint64()
 }
