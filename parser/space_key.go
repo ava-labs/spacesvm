801b47117b19cd684eb48eda22f0ed35b49fbfda
Patrick O'Grady <prohb125@gmail.com>
2022-01-13 21:58:40 -0800

    cleanup parser

---
+++ parser/space_key.go
@@ -0,0 +1,51 @@
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// Package parser defines storage key parsing operations.
+package parser
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+const (
+	MaxIdentifierSize = 256
+	Delimiter         = "/"
+)
+
+var (
+	ErrInvalidContents = errors.New("spaces and keys must be ^[a-z0-9]{1,256}$")
+	ErrInvalidPath     = errors.New("path is not of the form space/key")
+
+	reg *regexp.Regexp
+)
+
+func init() {
+	reg = regexp.MustCompile("^[a-z0-9]{1,256}$")
+}
+
+// CheckContents returns an error if the identifier (space or key) format is invalid.
+func CheckContents(identifier string) error {
+	if !reg.MatchString(identifier) {
+		return ErrInvalidContents
+	}
+	return nil
+}
+
+func ResolvePath(path string) (space string, key string, err error) {
+	segments := strings.Split(path, Delimiter)
+	if len(segments) != 2 {
+		return "", "", ErrInvalidPath
+	}
+	space = segments[0]
+	if err := CheckContents(space); err != nil {
+		return "", "", err
+	}
+	key = segments[1]
+	if err := CheckContents(key); err != nil {
+		return "", "", err
+	}
+	return
+}
---
+++ parser/space_key_test.go
@@ -0,0 +1,122 @@
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package parser
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestCheckContents(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		identifier string
+		err        error
+	}{
+		{
+			identifier: "foo",
+			err:        nil,
+		},
+		{
+			identifier: "asjdkajdklajsdklajslkd27137912kskdfoo",
+			err:        nil,
+		},
+		{
+			identifier: "0xasjdkajdklajsdklajslkd27137912kskdfoo",
+			err:        nil,
+		},
+		{
+			identifier: "",
+			err:        ErrInvalidContents,
+		},
+		{
+			identifier: "Ab1",
+			err:        ErrInvalidContents,
+		},
+		{
+			identifier: "a a",
+			err:        ErrInvalidContents,
+		},
+		{
+			identifier: "a/a",
+			err:        ErrInvalidContents,
+		},
+		{
+			identifier: "😀",
+			err:        ErrInvalidContents,
+		},
+		{
+			identifier: strings.Repeat("a", MaxIdentifierSize+1),
+			err:        ErrInvalidContents,
+		},
+	}
+	for i, tv := range tt {
+		err := CheckContents(tv.identifier)
+		if !errors.Is(err, tv.err) {
+			t.Fatalf("#%d: err expected %v, got %v", i, tv.err, err)
+		}
+	}
+}
+
+func TestResolvePath(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		path string
+		err  error
+
+		space string
+		key   string
+	}{
+		{
+			path:  "foo/bar",
+			err:   nil,
+			space: "foo",
+			key:   "bar",
+		},
+		{
+			path:  "foo1287391723981273891723981739817398192/barasjdkajdlasjdl",
+			err:   nil,
+			space: "foo1287391723981273891723981739817398192",
+			key:   "barasjdkajdlasjdl",
+		},
+		{
+			path: "",
+			err:  ErrInvalidPath,
+		},
+		{
+			path: "foo/",
+			err:  ErrInvalidContents,
+		},
+		{
+			path: "foo///",
+			err:  ErrInvalidPath,
+		},
+		{
+			path: "/test",
+			err:  ErrInvalidContents,
+		},
+		{
+			path: "aajsdklasd82u8931H/bar",
+			err:  ErrInvalidContents,
+		},
+	}
+	for i, tv := range tt {
+		space, key, err := ResolvePath(tv.path)
+		if !errors.Is(err, tv.err) {
+			t.Fatalf("#%d: err expected %v, got %v", i, tv.err, err)
+		}
+		if tv.err != nil {
+			continue
+		}
+		if space != tv.space {
+			t.Fatalf("#%d: expected %v, got %v", i, tv.space, space)
+		}
+		if key != tv.key {
+			t.Fatalf("#%d: expected %v, got %v", i, tv.key, key)
+		}
+	}
+}
