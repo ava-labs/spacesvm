9aa89ce246eba0470fdecdaedf36cfca164e7b63
Patrick O'Grady <prohb125@gmail.com>
2022-01-15 17:56:27 -0800

    commit genesis in a batch

--- chain/genesis.go
+++ chain/genesis.go
@@ -6,8 +6,10 @@ package chain
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/ava-labs/avalanchego/database"
+	"github.com/ava-labs/avalanchego/database/versiondb"
 	"github.com/ava-labs/avalanchego/utils/units"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -118,36 +120,44 @@ func (g *Genesis) Verify() error {
 	return nil
 }
 
-func (g *Genesis) Load(db database.KeyValueWriter, airdropData []byte) error {
+func (g *Genesis) Load(db database.Database, airdropData []byte) error {
+	start := time.Now()
+	defer func() {
+		log.Debug("loaded genesis allocations", "t", time.Since(start))
+	}()
+
+	vdb := versiondb.New(db)
 	if len(g.AirdropHash) > 0 {
 		h := common.BytesToHash(crypto.Keccak256(airdropData)).Hex()
 		if g.AirdropHash != h {
 			return fmt.Errorf("expected standard allocation %s but got %s", g.AirdropHash, h)
 		}
 
-		standardAllocation := []*Airdrop{}
-		if err := json.Unmarshal(airdropData, &standardAllocation); err != nil {
+		airdrop := []*Airdrop{}
+		if err := json.Unmarshal(airdropData, &airdrop); err != nil {
 			return err
 		}
 
-		for _, alloc := range standardAllocation {
-			if err := SetBalance(db, alloc.Address, g.AirdropUnits); err != nil {
+		for _, alloc := range airdrop {
+			if err := SetBalance(vdb, alloc.Address, g.AirdropUnits); err != nil {
 				return fmt.Errorf("%w: addr=%s, bal=%d", err, alloc.Address, g.AirdropUnits)
 			}
 		}
 		log.Debug(
 			"applied airdrop allocation",
-			"hash", h, "addrs", len(standardAllocation), "balance", g.AirdropUnits,
+			"hash", h, "addrs", len(airdrop), "balance", g.AirdropUnits,
 		)
 	}
 
 	// Do custom allocation last in case an address shows up in standard
 	// allocation
 	for _, alloc := range g.CustomAllocation {
-		if err := SetBalance(db, alloc.Address, alloc.Balance); err != nil {
+		if err := SetBalance(vdb, alloc.Address, alloc.Balance); err != nil {
 			return fmt.Errorf("%w: addr=%s, bal=%d", err, alloc.Address, alloc.Balance)
 		}
 		log.Debug("applied custom allocation", "addr", alloc.Address, "balance", alloc.Balance)
 	}
-	return nil
+
+	// Commit as a batch to improve speed
+	return vdb.Commit()
 }
