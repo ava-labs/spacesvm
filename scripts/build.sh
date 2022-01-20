0bed78ce902c189611a11007abcadd48acaeb193
Patrick O'Grady <prohb125@gmail.com>
2022-01-19 16:43:13 -0800

    cleanup build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,27 +9,14 @@ if ! [[ "$0" =~ scripts/build.sh ]]; then
   exit 255
 fi
 
-# Load the constants
-# Set the PATHS
-GOPATH="$(go env GOPATH)"
-
 # Set default binary directory location
-binary_directory="$GOPATH/src/github.com/ava-labs/avalanchego/build/plugins"
 name="sqja3uK17MJxfC7AN8nGadBw9JK5BcrsNwNynsqP5Gih8M5Bm"
 
-if [[ $# -eq 1 ]]; then
-    binary_directory=$1
-elif [[ $# -eq 2 ]]; then
-    binary_directory=$1
-    name=$2
-elif [[ $# -ne 0 ]]; then
-    echo "Invalid arguments to build spacesvm. Requires either no arguments (default) or one arguments to specify binary location."
-    exit 1
-fi
-
 # Build spacesvm, which is run as a subprocess
-echo "Building spacesvm in $binary_directory/$name"
-go build -o "$binary_directory/$name" ./cmd/spacesvm
-
 mkdir -p ./build
+
+echo "Building spacesvm in ./build/$name"
+go build -o ./build/$name ./cmd/spacesvm
+
+echo "Building spaces-cli in ./build/spaces-cli"
 go build -o ./build/spaces-cli ./cmd/spaces-cli
